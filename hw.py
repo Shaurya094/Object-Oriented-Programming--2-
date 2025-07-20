d3c02c605a926ef0f1207a677e90be55d4fbd625
Shaurya094 <Shauryauprari094@gmail.com>
2025-07-20 17:39:15 +1000

    hw

---
+++ hw.py
@@ -0,0 +1,5 @@
+r = int(input("Enter the radius of your circle: "))
+
+area = 3.14 * (r*r)
+circumfrence = 2 * 3.14 * r
+print("The circumfrence is {} and the area is {}." .format(circumfrence, area))
